c320d74af91a1eaf63c991647a35c003cf733b8b
Andrey Zakhryamin <azakhryamin@tinkoff.ru>
2021-08-17 15:01:42 +0300

    Initial commit

---
+++ src/main/java/com/example/sirius/lecture/LectureApplication.java
@@ -0,0 +1,14 @@
+package com.example.sirius.lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LectureApplication.class, args);
+	}
+
+}
